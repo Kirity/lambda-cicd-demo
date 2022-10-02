0931a8ec35cfdccfd4726672d97ff7b4ac4f5813
Kirity Rapuru <kirity.rapuru@ultratendency.com>
2022-10-02 13:25:42 +0200

    version v1

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,6 +4,7 @@ import boto3
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
+    print("version v1")
     print("S3 Buckets")
     print("-----------")
     for bucket in s3.buckets.all():
